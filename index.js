169be172438fbd2069e68122772c3c6f0b7e5e67
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-12 13:51:03 +0300

    added files

---
+++ index.js
@@ -0,0 +1,79 @@
+// Use string formatting to create a message that includes variables for a person's name and age. Print the formatted message.
+txt="john is " + "14 year 0ld"
+print(txt)
+// Explain the difference between `%` formatting and `str.format()` for string formatting in Python.
+
+// `%`
+b=7
+f=6
+l=b%f
+print(l)
+
+// str.format()
+t=8
+k=7
+f=8/7
+print(str.formart(f))
+
+
+
+
+
+// Given the string `"Python"`, retrieve and print the first and last characters using indexing.
+b="python"
+b[0]
+b[5]
+
+// Determine the length of the string `"Hello, World!"` in Python.
+p="hello World"
+print(len(p))
+// Given the string `"Programming"`, use slicing to extract the substring "gram."
+d="Programming"
+print=(d[3:7])
+// Slice the string `"abcdefgh"` to create a new string that includes only the characters from index 2 to index 5.
+t="abcdefgh"
+print=(t[2;6])
+
+// Use the `count()` method to find the number of occurrences of the letter "a" in the string `"banana"`.
+f='banana'
+d=f.count("a")
+print(d)
+// Convert the string `"hello"` to uppercase using the `upper()` method.
+
+d="hello"
+e=d.upper()
+print=e
+// Replace all occurrences of the word "apple" with "orange" in the string `"I have an apple and another apple."`
+g="i have an apple and another apple"
+h=g.replace("apple", "orange")
+print(h)
+// Concatenate the strings "Hello" and "World" to create the sentence "Hello World" in Python.
+
+w="Hello"
+h="World"
+t=w + " " + h
+print(w)
+
+// Given the string `"Python is fun"`, check if the string starts with "Python" using the `startswith()` method.
+
+t='Python is fun'
+txt=t.startsWith('python')
+print=(txt)
+
+// Convert the number `42` to a string in Python.
+num='42'
+print(type(num))
+
+//  Convert the string `"3.14"` to a floating-point number.
+num = '10.0'
+print=(type(num))
+// Find the index of the first occurrence of the letter "o" in the string `"Python"`.
+g="python"
+print(g.index(o))
+// Check if the substring "world" is present in the string `"Hello, World!"` using the `in` keyword.
+
+word="Hello World"
+if "World" in word:
+print("yes")
+
+
